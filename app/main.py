817f1a06ba84ca7acacde55ac01088f6fff86341
vivek-saara <vivek.y@saara.io>
2024-07-27 14:23:21 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -12,7 +12,18 @@ def main():
     server_socket = socket.create_server(address=address, reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
     # server_socket.bind(address)
-    conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n\r\n", socket.MSG_WAITALL)
+    request = conn.recv(1024)
+    request_line = request.decode('utf-8').split("\r\n")[0]
+    path = request_line.split(" ")[1]
+    if path == "/":
+        not_found = False
+    else:
+        not_found = True
+    print(path)
+    if not_found:
+        conn.sendall(b"HTTP/1.1 404 NOT FOUND\r\n\r\n", socket.MSG_WAITALL)
+    else:
+        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n\r\n", socket.MSG_WAITALL)
 
 if __name__ == "__main__":
     main()

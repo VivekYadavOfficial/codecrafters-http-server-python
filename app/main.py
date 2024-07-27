bf16c157c56ad5a30808135df2668492bf7a3671
vivek-saara <vivek.y@saara.io>
2024-07-27 15:29:40 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -11,11 +11,22 @@ def main():
     address = ("localhost", 4221)
     server_socket = socket.create_server(address=address, reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
-    # server_socket.bind(address)
     request = conn.recv(1024)
     request_line = request.decode('utf-8').split("\r\n")[0]
     path = request_line.split(" ")[1]
     print(path)
+    # check if path is user-agent
+    if path == "/user-agent":
+        # extract header from request
+        headers = request.decode('utf-8').split("\r\n\r\n")[0].split("\r\n")[1:]
+        # loop through and find which header is user-agent
+        for header in headers:
+            if header.split(":")[0].lower() == 'user-agent':
+                user_agent = header.split(":")[1]
+                user_agent = user_agent.strip()
+                content_length = len(user_agent)
+                conn.sendall(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + str(content_length).encode() + b"\r\n\r\n" + user_agent.encode() + b"\r\n", socket.MSG_WAITALL)
+
     if path == "/":
         r_str = "/"
         content_length = len(r_str)
